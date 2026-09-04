49f7e4ecec4efba49c0333deb4adc270ca7e6338
Thales Wallison (TW Da Vinci) <thales.dvf@gmail.com>
2026-09-04 07:35:35 -0300

    feat: add Biblical Languages app entrypoint

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+import 'biblical_languages/ui/biblical_languages_catalog_screen.dart';
+
+void main() {
+  runApp(const HnkBiblicalLanguagesApp());
+}
+
+class HnkBiblicalLanguagesApp extends StatelessWidget {
+  const HnkBiblicalLanguagesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'HNK Biblical Languages',
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color(0xFF0057D8),
+        ),
+        scaffoldBackgroundColor: const Color(0xFFF8FAFC),
+      ),
+      home: const BiblicalLanguagesCatalogScreen(),
+    );
+  }
+}
